3bfb790d9ee817f4f23f962bb4b6f7b2c0ee03d4
jolojo500 <193266106+jolojo500@users.noreply.github.com>
2025-12-23 18:37:32 -0500

    working copying worlds

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ string worldsPath = Path.Combine(
     "Worlds"
 );
 
+string stagingPath = Path.Combine(Directory.GetCurrentDirectory(),"staging");
+
+Directory.CreateDirectory(stagingPath); //in case it's gone better be safe than sorry I suppose
+
 if (!Directory.Exists(worldsPath))
 {
     Console.WriteLine("Worlds folder not found.");
@@ -26,7 +30,14 @@ var worlds = files
     {
         var parts = name.Split(".");
         return parts[0];
-    });
+    })
+    .ToList();
+
+if (worlds.Count == 0)
+{
+    Console.WriteLine("No worlds found.");
+    return;
+}
 
 Console.WriteLine("Detected worlds:\n");
 foreach (var world in worlds)
@@ -39,3 +50,22 @@ foreach (var world in worlds)
     }
     Console.WriteLine();
 }
+
+var selectedWorld = worlds[0]; // testing, will change to wtv gui interaction
+string worldName = selectedWorld.Key;
+string worldStagingDir = Path.Combine(stagingPath, worldName);
+
+Directory.CreateDirectory(worldStagingDir);
+
+Console.WriteLine($"Copying world '{worldName} to staging...\n");
+
+foreach(var file in selectedWorld)
+{
+    string source = Path.Combine(worldsPath, file);
+    string destination = Path.Combine(worldStagingDir, file);
+
+    File.Copy(source, destination, overwrite: true);
+    Console.WriteLine($"Copied: {file}");
+}
+
+Console.WriteLine("\nDone");
